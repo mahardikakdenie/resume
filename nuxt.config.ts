1052e61927d3bd8864cc5fee5278571c6ce5a1bd
MahardiikaKD <dikamahar884@gmail.com>
2024-08-17 16:16:59 +0700

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,4 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
-  css: [
-    'https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap'
-  ],
 })

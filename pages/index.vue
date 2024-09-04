0568c51a15469645aa7b3a9b91547525786053ab
MahardiikaKD <dikamahar884@gmail.com>
2024-09-04 22:21:55 +0700

    use head meta

--- pages/index.vue
+++ pages/index.vue
@@ -10,17 +10,12 @@ A Software Developer From Bandung
 I've been working in software engineering for more than 3 years. I have good knowledge of frontend and backend.
 I always try my best when building or developing software, so I always keep my code as tidy as possible and refactor periodically. I prefer to break my code into smaller chunks so it's easier to read. My skillset includes:`;
 
-useHead({
-	title: 'Mahardika D Kessuma - about',
-	meta: [
-		{
-			name: 'description',
-			content: META_DESCRIPTION,
-		},
-	],
-	bodyAttrs: {
-		class: 'test',
-	},
-	script: [{ innerHTML: "console.log('Hello world')" }],
+const TITLE = 'MAHARDIKA KESSUMA DENIE - PROFILE';
+
+useSeoMeta({
+	title: TITLE,
+	ogTitle: TITLE,
+	description: META_DESCRIPTION,
+	ogDescription: META_DESCRIPTION,
 });
 </script>

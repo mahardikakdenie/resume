f3feaa979fdefefd117573a278ef4fcd8a7136f6
mahardikakdenie <kucingemas744@gmail.com>
2026-02-05 16:08:57 +0700

    chore(nuxt-config): update nuxt configure projects

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  devtools: { enabled: true, },
+  devtools: { enabled: true },
   devServer: {
     port: 3001,
+  },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' }
   },
   modules: ["@nuxtjs/tailwindcss", "nuxt-aos"],
 })
